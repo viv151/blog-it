795ae129def11c6de24f629856f869770f12610e
viv151 <vivekhjat@gmail.com>
2025-10-04 02:53:21 +0530

    minor-fixes

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -84,12 +84,12 @@ const Blog = () => {
         </h1>
         <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
         <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">
-          Michel Brown
+          Vivek Jat
         </p>
       </div>
 
       <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
-        <img src={data.image} alt="" className="rounded-3xl mb-5" />
+        <img src={data.image} alt="" className="rounded-3xl mb-5 blog-img" />
 
         <div
           className="rich-text max-w-3xl mx-auto"
